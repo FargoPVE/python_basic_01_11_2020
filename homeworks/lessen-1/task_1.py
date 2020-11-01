9b074d84458b8e86bf1fd95da374be5f4b67838b
FargoPVE <darksnowpve@gmail.com>
2020-11-01 23:52:29 +0700

    Task-1 completed

---
+++ homeworks/lessen-1/task_1.py
@@ -0,0 +1,20 @@
+"""
+1. Поработайте с переменными,
+создайте несколько, выведите на экран,
+запросите у пользователя несколько чисел и строк и сохраните в переменные,
+выведите на экран.
+"""
+
+my_first_type = "Кажется, это строка"
+my_second_type = 15
+my_third_type = 18.9
+my_fourth_type = False
+print(my_first_type)
+print(my_second_type)
+print(my_third_type)
+print(my_fourth_type)
+
+user_name = input("Введите Ваше имя: ")
+user_age = int(input("Введите Ваш возраст: "))
+print("Вас зовут: ", user_name)
+print("Ваш возраст: ", user_age)
