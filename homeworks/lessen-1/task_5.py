893987ea9ff8fb21cf9e1fd930d3759038e29b94
FargoPVE <darksnowpve@gmail.com>
2020-11-02 01:29:29 +0700

    Task-5 completed

--- homeworks/lessen-1/task_5.py
+++ homeworks/lessen-1/task_5.py
@@ -7,3 +7,22 @@
 Далее запросите численность сотрудников фирмы и определите прибыль фирмы в расчете на одного сотрудника.
 """
 
+user_revenue = int(input("Введите значение выручки: "))
+user_cost = int(input("Введите значение издержки: "))
+
+if user_revenue > user_cost:
+    print("Ваша фирма работает работает с прибылью")
+    user_proceeds = user_revenue - user_cost
+    user_profitability = user_revenue // user_proceeds
+    print("Рентабельность Вашей фирмы равна: ")
+elif user_revenue < user_cost:
+    print("Ваша фирма работает с убытками")
+elif user_revenue == user_cost:
+    print("Ваша фирма работает в ноль (прибыль равна издержкам)")
+else:
+    print("Ваши значения не соответствуют условиям программы")
+
+user_staff = int(input("Введите численность сотрудников Вашей фирмы: "))
+user_profit = user_revenue / user_staff
+print("Прибыль на одного сотрудника равна: ", user_profit)
+
